e77b2905d048ba083ed09da64dcbb8d2e4ba2cb3
kangsoekme <sukmajati.abdullah@gmail.com>
2026-08-09 16:47:33 +0700

    Fix pg deprecation warning by using native Prisma Client engine instead of adapter-pg

--- src/application/database.js
+++ src/application/database.js
@@ -1,12 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const { Pool } = await import("pg");
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
-export const prismaClient = new PrismaClient({ adapter });
+export const prismaClient = new PrismaClient();
