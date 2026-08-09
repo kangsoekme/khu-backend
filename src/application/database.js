e1116ee26a4a3659b1eabfe808138de8258dd9a1
kangsoekme <sukmajati.abdullah@gmail.com>
2026-08-09 16:55:14 +0700

    Revert PrismaClient change to fix backend crash (502 Bad Gateway) causing CORS errors

--- src/application/database.js
+++ src/application/database.js
@@ -1,3 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prismaClient = new PrismaClient();
+const { Pool } = await import("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prismaClient = new PrismaClient({ adapter });
